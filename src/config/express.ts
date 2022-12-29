39570bb92899e067b3c85d915209aa52e8bb458f
Edouard Misset <edouard.misset@intest.info>
2022-12-29 15:09:32 +0100

    Added compression

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import compression from 'compression'
 
 const createServer = (): express.Application => {
   const app = express()
 
   app.use(cors())
+  app.use(compression())
 
   app.use(express.urlencoded({ extended: true }))
   app.use(express.json())
